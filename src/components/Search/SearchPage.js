4f48f7a52a0681041f2e205c1b2442d4ee3232b0
musta7188 <musta7188@hotmail.it>
2020-05-07 01:17:05 +0100

    voice recognition works"

--- src/components/Search/SearchPage.js
+++ src/components/Search/SearchPage.js
@@ -1,62 +1,86 @@
-import React, { useState, useEffect }from 'react';
-import ReactSearchBox from 'react-search-box'
-import SearchContainer from './SearchContainer'
-import {connect} from 'react-redux'
- function SearchPage(props) {
-  const {classes} = props
-  const {SongsSearched} = props
+import React, { useState, useEffect } from "react";
+import ReactSearchBox from "react-search-box";
+import SearchContainer from "./SearchContainer";
+import { connect } from "react-redux";
+import MicOffIcon from "@material-ui/icons/MicOff";
+import MicIcon from "@material-ui/icons/Mic";
 
-  const [value, setValue] = useState("")
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-  ///fetch from the api songs, artist or album matching the value of the input 
+const recognition = new SpeechRecognition();
+
+recognition.continous = true;
+recognition.interimResults = true;
+recognition.lang = "en-US";
+
+function SearchPage(props) {
+  const { SongsSearched } = props;
+
+  const [value, setValue] = useState("");
+
+  const [listening, setListening] = useState(false);
+
+  ///fetch from the api songs, artist or album matching the value of the input
   const getSearchedItems = () => {
+    fetch(`https://deezerdevs-deezer.p.rapidapi.com/search/track?q=${value}`, {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+        "x-rapidapi-key": "178f0cd1fbmsh29f81f40b999084p1211d1jsneb0d0e6e0aaf",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((obj) => SongsSearched(obj.data));
+  };
 
-      fetch(`https://deezerdevs-deezer.p.rapidapi.com/search/track?q=${value}`, {
-        "method": "GET",
-        "headers": {
-          "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-          "x-rapidapi-key": "178f0cd1fbmsh29f81f40b999084p1211d1jsneb0d0e6e0aaf"
-   } }).then(resp => resp.json()).then(obj => SongsSearched(obj.data))
+  const handleListen = () => {
+    listening ? recognition.start() : recognition.stop();
+  };
 
-  }
-
+  recognition.onresult = (e) => {
+  
+    handelChance(e.results[0][0].transcript);
+  };
 
   const handelChance = (value) => {
-    setValue(value)
-    getSearchedItems()
-  }
-
+    setValue(value);
+    getSearchedItems();
+  };
 
   return (
-      <>
-    <ReactSearchBox
-          placeholder="Search Songs"
-          onChange={(value) => handelChance(value)}
-          fuseConfigs={{
-            threshold: 0.05,
-          }}
-          value={value}
-        />
-        
-        <br></br>
-  
-      <SearchContainer/>
-        </>
+    <>
+      <ReactSearchBox
+        placeholder="Search Songs"
+        onChange={(value) => handelChance(value)}
+        fuseConfigs={{
+          threshold: 0.05,
+        }}
+        value={value}
+        id="speechinput"
+        x-webkit-speech
+      />
 
-  )
+      <button
+        onClick={() => {
+          setListening(!listening);
+          handleListen();
+        }}
+      >
+        {listening ? <MicIcon /> : <MicOffIcon />}
+      </button>
 
+      <br></br>
+
+      <SearchContainer />
+    </>
+  );
 }
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    SongsSearched: songs => dispatch({type: "SELECTED_SONGS", payload: {songs: songs}})
-  }
-}
+    SongsSearched: (songs) =>
+      dispatch({ type: "SELECTED_SONGS", payload: { songs: songs } }),
+  };
+};
 
-
-
-export default connect(null, mapDispatchToProps) (SearchPage);
-
-
-
-
+export default connect(null, mapDispatchToProps)(SearchPage);
